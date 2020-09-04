bc67a262555a42b50e2f838818079a738e5a2683
soho.chan <soho.chan@cs.hkbn.com.hk>
2020-09-04 18:57:49 +0800

    add controller.

--- src/main/java/com/soho/template/controller/TestController.java
+++ src/main/java/com/soho/template/controller/TestController.java
@@ -1,15 +1,43 @@
 package com.soho.template.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 /**
  * @author soho.chan
  * @date 4/9/2020 18:12
  * @description TODO
  */
-@RestController
+@RestController("data")
 public class TestController {
+    private static final int DATA_SIZE = 10;
+    private static List<HashMap<String, String>> data = new ArrayList<>();
 
+    {
+        for (int i = 0; i < DATA_SIZE; i++) {
+            HashMap<String, String> map = new HashMap<>();
+            map.put("id", String.valueOf(i));
+            map.put("name", "user" + i);
+            data.add(map);
+        }
+    }
 
+    @GetMapping("list")
+    public List<HashMap<String, String>> getAll() {
+        return data;
+    }
+
+    @GetMapping("{id}")
+    public HashMap<String, String> getById(@PathVariable String id) {
+        if (id != null) {
+            for (HashMap<String, String> map: data) {
+                if (id.equals(map.get("id"))) {
+                    return map;
+                }
+            }
+        }
+        return null;
+    }
 
 }
